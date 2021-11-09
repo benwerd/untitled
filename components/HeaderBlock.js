bffade3f1e6e8899e1ea5f608746647b16cb3d17
Ben Werdmuller <ben@benwerd.com>
2021-11-08 22:51:59 -0500

    Header block with web monetization block

--- components/HeaderBlock.js
+++ components/HeaderBlock.js
@@ -14,6 +14,7 @@ export default function HeaderBlock({ pageTitle }) {
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
             <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
             <link rel="manifest" href="/site.webmanifest"/>
+            <meta name="monetization" content="$ilp.uphold.com/7yhraRBZn2GY"/>
         </Head>
     )
 }
